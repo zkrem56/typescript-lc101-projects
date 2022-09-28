4fb20adb065750d2d2f0084bb6dd83406cb46992
zkrem56 <zkremer702@gmail.com>
2022-09-28 14:18:38 -0500

    Completed part 2 in exercise

--- exercises/parts1-5.js
+++ exercises/parts1-5.js
@@ -1,52 +1,25 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
-
 // Part 1: Declare (5) Variables With Type
-let spacecraftName:string = 'Determination';
-let speedMph:number = 17500;
-let kilometersToMars:number = 225000000;
-let kilometersToTheMoon:number = 384400;
-let milesPerKilometer:number = 0.621;
-
+var spacecraftName = 'Determination';
+var speedMph = 17500;
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
+var milesPerKilometer = 0.621;
 // Part 2: Print Days to Mars
-
-let milesToMars:number = kilometersToMars * milesPerKilometer;
-let hoursToMars:number = milesToMars / speedMph;
-let daysToMars:number = hoursToMars / 24;
-
+var milesToMars = kilometersToMars * milesPerKilometer;
+var hoursToMars = milesToMars / speedMph;
+var daysToMars = hoursToMars / 24;
 // Code an output statement here (use a template literal):
-
-console.log(`${spacecraftName} would take ${daysToMars} to get to Mars.`);
-
+console.log("".concat(spacecraftName, " would take ").concat(daysToMars, " to get to Mars."));
 // Part 3: Create a Function ("getDaysToLocation")
-
-
-
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
-
-
 // Part 4: Create a Spacecraft Class
-
-
-
-
 // Create an instance of the class here:
-
-
-
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-
-
-
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
-
-
 // Add the printDaysToLocation function to the Spacecraft class.
-
 // Paste in the code from step 6 here:
